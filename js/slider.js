d5109148c2f77f51862f06edf85bdf270aff2c44
kato <48169123+katoyuko@users.noreply.github.com>
2022-02-15 02:48:51 +0900

    ギャラリーのスライダーを追加

--- js/slider.js
+++ js/slider.js
@@ -1,8 +1,26 @@
 $(function () {
+  // ファーストビュー
   $(".slider").slick({
     arrows: false,
     autoplay: true,
     dots: true,
     pauseOnHover: false, //ホバーしたときにスライドを一時停止しない
+  });
+
+  // ギャラリー
+  $(".gallery").slick({
+    autoplay: true,
+    pauseOnHover: false,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slideToScroll: 1,
+        },
+      },
+    ],
   });
 });
